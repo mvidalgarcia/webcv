89bd4c9a6c8d66a03635d8149d0ae781b61dc119
Marco Vidal García <marcovidal67@gmail.com>
2016-05-09 22:53:27 +0100

    Dynamic addtions and removals in /edit page done

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -57,5 +57,129 @@ webcvControllers.controller('EditCVCtrl',
     $scope.deleteSkillKeyword = function(skill_idx, keyword_idx) {
       $scope.cv.skills[skill_idx].keywords.splice(keyword_idx, 1);
     }
+
+    /* Add experience dynamically */
+    $scope.addExperience = function() {
+      $scope.cv.work.push({company: "", position: "", website: "", startDate: "",
+       endDate: "", summary: "", highlights: [""]});
+    }
+
+    /* Delete experience dynamically */
+    $scope.deleteExperience = function(index) {
+      $scope.cv.work.splice(index, 1);
+    }
+
+    /* Add experience highlight dynamically */
+    $scope.addExpHighlight = function(exp_idx) {
+      $scope.cv.work[exp_idx].highlights.push("");
+    }
+
+    /* Delete experience highlight dynamically */
+    $scope.deleteExpHighlight = function(exp_idx, hl_idx) {
+      $scope.cv.work[exp_idx].highlights.splice(hl_idx, 1);
+    }
+
+    /* Add volunteering dynamically */
+    $scope.addVolunteering = function() {
+      $scope.cv.volunteer.push({organization: "", position: "", website: "", startDate: "",
+       endDate: "", summary: "", highlights: [""]});
+    }
+
+    /* Delete volunteering dynamically */
+    $scope.deleteVolunteering = function(index) {
+      $scope.cv.volunteer.splice(index, 1);
+    }
+
+    /* Add volunteering highlight dynamically */
+    $scope.addVolHighlight = function(vol_idx) {
+      $scope.cv.volunteer[vol_idx].highlights.push("");
+    }
+
+    /* Delete volunteering highlight dynamically */
+    $scope.deleteVolHighlight = function(vol_idx, hl_idx) {
+      $scope.cv.volunteer[vol_idx].highlights.splice(hl_idx, 1);
+    }
+
+    /* Add education dynamically */
+    $scope.addEducation = function() {
+      $scope.cv.education.push({institution: "", area: "", studyType: "", startDate: "",
+      endDate: "", gpa: "", courses: [""]});
+    }
+
+    /* Delete education dynamically */
+    $scope.deleteEducation = function(index) {
+      $scope.cv.education.splice(index, 1);
+    }
+
+    /* Add education course dynamically */
+    $scope.addEduCourse = function(edu_idx) {
+      $scope.cv.education[edu_idx].courses.push("");
+    }
+
+    /* Delete education course dynamically */
+    $scope.deleteEduCourse = function(edu_idx, hl_idx) {
+      $scope.cv.education[edu_idx].courses.splice(hl_idx, 1);
+    }
+
+    /* Add award dynamically */
+    $scope.addAward = function() {
+      $scope.cv.awards.push({title: "", date: "", awarder: "",summary: ""});
+    }
+
+    /* Delete award dynamically */
+    $scope.deleteAward = function(index) {
+      $scope.cv.awards.splice(index, 1);
+    }
+
+    /* Add publication dynamically */
+    $scope.addPublication = function() {
+      $scope.cv.publications.push({name: "", publisher: "", releaseDate: "", website: "", summary: ""});
+    }
+
+    /* Delete publication dynamically */
+    $scope.deletePublication = function(index) {
+      $scope.cv.publications.splice(index, 1);
+    }
+
+    /* Add language dynamically */
+    $scope.addLanguage = function() {
+      $scope.cv.languages.push({name: "", level: ""});
+    }
+
+    /* Delete publication dynamically */
+    $scope.deleteLanguage = function(index) {
+      $scope.cv.languages.splice(index, 1);
+    }
+
+    /* Add interest dynamically */
+    $scope.addInterest = function() {
+      $scope.cv.interests.push({name: "", keywords: [""]});
+    }
+
+    /* Delete interest dynamically */
+    $scope.deleteInterest = function(index) {
+      $scope.cv.interests.splice(index, 1);
+    }
+
+    /* Add interest keyword dynamically */
+    $scope.addIntKeyword = function(int_idx) {
+      $scope.cv.interests[int_idx].keywords.push("");
+    }
+
+    /* Delete interest keyword dynamically */
+    $scope.deleteIntKeyword = function(int_idx, kw_idx) {
+      $scope.cv.interests[int_idx].keywords.splice(kw_idx, 1);
+    }
+
+    /* Add reference dynamically */
+    $scope.addReference = function() {
+      $scope.cv.references.push({name: "", reference: ""});
+    }
+
+    /* Delete reference dynamically */
+    $scope.deleteReference = function(index) {
+      $scope.cv.references.splice(index, 1);
+    }
+
   }
 );
